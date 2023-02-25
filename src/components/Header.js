8647c168d7292e978599740e80123ce920c950b3
King Prakash <prakash@upixels.in>
2023-02-25 15:17:05 +0530

    added logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <header className="App-header p-3 border bg-blue-100">
-  Header component
+  <div><img width="80" src="https://upload.wikimedia.org/wikipedia/en/thumb/8/8c/Trello_logo.svg/1280px-Trello_logo.svg.png" alt="logo"/> </div>
     </header>
   );
 };
